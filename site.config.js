5f5806eac0ad2f1e7bd78125ecf2a8d0db896103
诸葛子房 <xiaoqiu2017wy@163.com>
2024-01-12 19:21:11 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -5,6 +5,7 @@ const CONFIG = {
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "frontend developer",
     bio: "I develop everything using node.",
+    email: "xiaoqiu2017wy@163.com",
     github: "zhugezifang",
     instagram: "",
   },
