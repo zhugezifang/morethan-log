f8b5c25f11c6673bee598d9ce0077c46d963f88c
诸葛子房 <xiaoqiu2017wy@163.com>
2024-01-12 19:15:00 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,13 +1,11 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "诸葛子房",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "frontend developer",
     bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    github: "zhugezifang",
     instagram: "",
   },
   projects: [
